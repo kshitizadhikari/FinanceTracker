92c472e2922d87ebbafd1b9bc8bcfde8855d03cb
kshitiz <adhikarikshitiz12@gmail.com>
2024-06-27 09:57:51 +0545

    use collectstatic to collect all static files in the STATIC_ROOT

--- finance_tracker/finance_tracker/settings.py
+++ finance_tracker/finance_tracker/settings.py
@@ -143,7 +143,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'finance_tracker/static')
 ]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', '/static')
-STATIC_ROOT = "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
---
+++ finance_tracker/staticfiles/js/main.js
@@ -0,0 +1,1 @@
+console.log("Testing from main.js");
